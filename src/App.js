0609d3522908c2c4f6870f3e9ad8f22db0a075d7
Daniel <dancardosobr@gmail.com>
2020-08-14 00:39:46 +0100

    chore: init project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <>
+          <Header />
+          <Routes />
+          <GlobalStyle />
+        </>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
